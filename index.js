30e22cad47451d48b2e5e01fbfd138d8a1b9a4dc
RodrDevs80 <rodr26707@gmail.com>
2024-09-16 20:10:43 -0300

    amigos

---
+++ index.js
@@ -0,0 +1,6 @@
+const contenedor = document.getElementById('contenedor');
+const amigos = document.getElementById('amigos');
+
+amigos.addEventListener('click', () => {
+    contenedor.style.display = 'flex';
+})
